01f0d4fc6acad9be91d43e3a651a585c6180a165
Mark Farrell <markfarr2011@gmail.com>
2020-06-03 23:13:03 +0000

    Separate test utilities

--- test/Text/Parsing/Array/Repetition.purs
+++ test/Text/Parsing/Array/Repetition.purs
@@ -10,67 +10,67 @@ import Text.Parsing.Parser.String as S
 
 import Text.Parsing.Array.Repetition as R
 
-import Test.Text.Parsing as P
+import Text.Parsing.Expect as E
 
 n :: Int
 n = (-1)
 
 least :: Effect Unit
 least = do
-  _ <- P.success "a"   (R.least n (S.char 'a'))
-  _ <- P.success "a"   (R.least 0 (S.char 'a'))
-  _ <- P.success "a"   (R.least 1 (S.char 'a'))
-  _ <- P.failure "a"   (R.least 2 (S.char 'a'))
-  _ <- P.success "aa"  (R.least 2 (S.char 'a'))
-  _ <- P.failure "a "  (R.least 2 (S.char 'a'))
-  _ <- P.success "aaa" (R.least 2 (S.char 'a'))
+  _ <- E.success "a"   (R.least n (S.char 'a'))
+  _ <- E.success "a"   (R.least 0 (S.char 'a'))
+  _ <- E.success "a"   (R.least 1 (S.char 'a'))
+  _ <- E.failure "a"   (R.least 2 (S.char 'a'))
+  _ <- E.success "aa"  (R.least 2 (S.char 'a'))
+  _ <- E.failure "a "  (R.least 2 (S.char 'a'))
+  _ <- E.success "aaa" (R.least 2 (S.char 'a'))
   pure unit
 
 exact :: Effect Unit
 exact = do
-  _ <- P.failure "a"   (R.exact n (S.char 'a'))
-  _ <- P.failure "a"   (R.exact 0 (S.char 'a'))
-  _ <- P.success "a"   (R.exact 1 (S.char 'a'))
-  _ <- P.failure "a"   (R.exact 2 (S.char 'a'))
-  _ <- P.success "aa"  (R.exact 2 (S.char 'a'))
-  _ <- P.failure "a "  (R.exact 2 (S.char 'a'))
-  _ <- P.failure "aaa" (R.exact 2 (S.char 'a'))
+  _ <- E.failure "a"   (R.exact n (S.char 'a'))
+  _ <- E.failure "a"   (R.exact 0 (S.char 'a'))
+  _ <- E.success "a"   (R.exact 1 (S.char 'a'))
+  _ <- E.failure "a"   (R.exact 2 (S.char 'a'))
+  _ <- E.success "aa"  (R.exact 2 (S.char 'a'))
+  _ <- E.failure "a "  (R.exact 2 (S.char 'a'))
+  _ <- E.failure "aaa" (R.exact 2 (S.char 'a'))
   pure unit
 
 most :: Effect Unit
 most = do
-  _ <- P.failure "a"   (R.most n (S.char 'a'))
-  _ <- P.failure "a"   (R.most 0 (S.char 'a'))
-  _ <- P.success "a"   (R.most 1 (S.char 'a'))
-  _ <- P.success "a"   (R.most 2 (S.char 'a'))
-  _ <- P.success "aa"  (R.most 2 (S.char 'a'))
-  _ <- P.success "a "  (R.most 2 (S.char 'a'))
-  _ <- P.failure "aaa" (R.most 2 (S.char 'a'))
-  _ <- P.success "aaa" (R.most 3 (S.char 'a'))
+  _ <- E.failure "a"   (R.most n (S.char 'a'))
+  _ <- E.failure "a"   (R.most 0 (S.char 'a'))
+  _ <- E.success "a"   (R.most 1 (S.char 'a'))
+  _ <- E.success "a"   (R.most 2 (S.char 'a'))
+  _ <- E.success "aa"  (R.most 2 (S.char 'a'))
+  _ <- E.success "a "  (R.most 2 (S.char 'a'))
+  _ <- E.failure "aaa" (R.most 2 (S.char 'a'))
+  _ <- E.success "aaa" (R.most 3 (S.char 'a'))
   pure unit
 
 range :: Effect Unit
 range = do
-  _ <- P.failure "a"  (R.range 0 n (S.char 'a'))
-  _ <- P.failure "a"  (R.range 0 0 (S.char 'a'))
-  _ <- P.failure "a"  (R.range 1 0 (S.char 'a'))
-  _ <- P.success "a"  (R.range n 0 (S.char 'a'))
-  _ <- P.success "a"  (R.range n 1 (S.char 'a'))
-  _ <- P.success "a"  (R.range 0 1 (S.char 'a'))
-  _ <- P.success "a"  (R.range 1 1 (S.char 'a'))
-  _ <- P.success "a"  (R.range 1 2 (S.char 'a'))
-  _ <- P.failure "aa" (R.range 0 1 (S.char 'a'))
-  _ <- P.success "aa" (R.range 0 2 (S.char 'a'))
-  _ <- P.success "aa" (R.range 1 2 (S.char 'a'))
-  _ <- P.success "aa" (R.range 2 2 (S.char 'a'))
+  _ <- E.failure "a"  (R.range 0 n (S.char 'a'))
+  _ <- E.failure "a"  (R.range 0 0 (S.char 'a'))
+  _ <- E.failure "a"  (R.range 1 0 (S.char 'a'))
+  _ <- E.success "a"  (R.range n 0 (S.char 'a'))
+  _ <- E.success "a"  (R.range n 1 (S.char 'a'))
+  _ <- E.success "a"  (R.range 0 1 (S.char 'a'))
+  _ <- E.success "a"  (R.range 1 1 (S.char 'a'))
+  _ <- E.success "a"  (R.range 1 2 (S.char 'a'))
+  _ <- E.failure "aa" (R.range 0 1 (S.char 'a'))
+  _ <- E.success "aa" (R.range 0 2 (S.char 'a'))
+  _ <- E.success "aa" (R.range 1 2 (S.char 'a'))
+  _ <- E.success "aa" (R.range 2 2 (S.char 'a'))
   pure unit
 
 until :: Effect Unit
 until = do
-  _ <- P.failure "a"  (R.until (S.char 'a') (S.char ' '))
-  _ <- P.success "a " (R.until (S.char 'a') (S.char ' '))
-  _ <- P.success "a " (R.until (S.satisfy (not <<< eq ' ')) (S.char ' '))
-  _ <- P.success "a " (R.until (S.char 'a') (S.char ' ') *> S.char (' '))
+  _ <- E.failure "a"  (R.until (S.char 'a') (S.char ' '))
+  _ <- E.success "a " (R.until (S.char 'a') (S.char ' '))
+  _ <- E.success "a " (R.until (S.satisfy (not <<< eq ' ')) (S.char ' '))
+  _ <- E.success "a " (R.until (S.char 'a') (S.char ' ') *> S.char (' '))
   pure unit
 
 main :: Effect Unit
